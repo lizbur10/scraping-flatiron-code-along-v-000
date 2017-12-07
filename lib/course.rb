76b52f4ecd26b464e26da02f0446c1aa0d66ae79
Liz Burton <liz.burton147@gmail.com>
2017-12-07 14:23:12 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,4 +1,10 @@
 class Course
 #take input from class Scraper and instantiate instances of Course
   attr_accessor :title, :schedule, :description
+  @all = []
+
+  def self.all
+    @all << self
+  end
+
 end
